773d315e422437512b65599b45230eb512b40100
Benjamin <benjamindiaocodes@gmail.com>
2019-03-09 17:53:33 +0800

    lesson 8

--- Phaser/app.js
+++ Phaser/app.js
@@ -1,18 +1,20 @@
-var game = new Phaser.game(800,600, Phaser.AUTO,'', {preload: preload, create: create, update: update});
+var game = new Phaser.Game(800,600, Phaser.AUTO,'', {preload: preload, create: create, update: update});
 var score = 0;
 var lives = 3;
-function preload(){}
+function preload(){
 	game.load.image('sky', 'assets/sky.png');
 	game.load.image('star', 'assets/star.png');
-	game.load.image('platform', 'platform/sky.png');
+	game.load.image('platform', 'assets/platform.png');
 	game.load.image('firstaid', 'assets/firstaid.png');
 
 	game.load.spritesheet('dude', 'assets/dude.png',32,48);
 	game.load.spritesheet('baddie', 'assets/baddie.png',32,32);
+}
+	
 
 function create(){
 
-	game.physics.start.System(Phaser.Physics.ARCADE);
+	game.physics.startSystem(Phaser.Physics.ARCADE);
 
 	//create sky
 	game.add.sprite(0, 0,'sky');
@@ -33,75 +35,129 @@ function create(){
 	//text
 	var style ={font: "bold 32px Arial", fill: "#fff"};
 
-	scorelabel = gmae.add.text(300,560,"Score: ",style);
-	scoretext = game.add.text(420, 560, score. style);
-	scorelabel.setShadow(3,3 'rgba(0,0,0,0.5)',2);
-	scoretext.setShadow(3,3 'rgba(0,0,0,0.5)',2);
+	scorelabel = game.add.text(300,560,"Score: ",style);
+	scoretext = game.add.text(420, 560, score, style);
+	scorelabel.setShadow(3,3, 'rgba(0,0,0,0.5)',2);
+	scoretext.setShadow(3,3,'rgba(0,0,0,0.5)',2);
 
 
-	liveslabel = gmae.add.text(300,560,"Lives: ",style);
-	livestext = game.add.text(420, 560, Lives. style);
-	liveslabel.setShadow(3,3 'rgba(0,0,0,0.5)',2);
-	livestext.setShadow(3,3 'rgba(0,0,0,0.5)',2);
+	liveslabel = game.add.text(10,10,"Lives: ",style);
+	livestext = game.add.text(120, 10, lives, style);
+	liveslabel.setShadow(3,3, 'rgba(0,0,0,0.5)',2);
+	livestext.setShadow(3,3, 'rgba(0,0,0,0.5)',2);
 
 	//player sprite
 	player  = game.add.sprite(10,300, 'dude');
 	  player.animations.add('left',[0,1,2,3],10, true );
 	  player.animations.add('right',[5,6,7,8],10, true );
-      game.physics.acrade.enable(player);
+      game.physics.arcade.enable(player);
 
 	  player.body.gravity.y =300;
 	  player.body.bounce.y= 0.2;
-	  player.body.colliderWorldBounds = true;
+	  player.body.collideWorldBounds = true;
 
 	  //enemeny sprite
-	  baddie = game.add.sprite( 20,200,baddie);
-	  enemeny aimmations.add("left",[0,1],10,true);
-	  enemeny aimmations.add("right",[2,3],10,true);
-	  game.physics.acrade.enable(enemeny);
+	  enemeny = game.add.sprite( 20,200,'baddie');
+	  enemeny.animations.add("left",[0,1],10,true);
+	  enemeny.animations.add("right",[2,3],10,true);
+	  game.physics.arcade.enable(enemeny);
 	  enemeny.body.gravity.y =500;
 	  enemeny.body.bounce.y=0.2;
-	  enemeny.body.colliderWorldBounds = true;
+	  enemeny.body.collideWorldBounds = true;
 
 	  //create star
 	  stars=game.add.physicsGroup();
-	  stars=enableBody = true;
+	  stars.enableBody = true;
 	  for(var i = 0; i < 12   ;i++){
-	  	var star=stars.create( i *70=0 ,0 , 'star');
+	  	var star=stars.create( i *70 ,0 , 'star');
         star.body.gravity.y=200;
-        star,body.bounce.y =Math.ramdom()*0.2+0.7
+        star.body.bounce.y =Math.random()*0.2+0.7
 	}
         //create keyboard entires
 
-        cursors= game.input.keyboard.createCursorkeys()
-        wKey=game.input.keyboard.addkey(Phaser.Keyboard.W)
-        aKey=game.input.keyboard.addkey(Phaser.Keyboard.a)
-        dKey=game.input.keyboard.addkey(Phaser.Keyboard.d)
+        cursors= game.input.keyboard.createCursorKeys()
+        wKey=game.input.keyboard.addKey(Phaser.Keyboard.W)
+        aKey=game.input.keyboard.addKey(Phaser.Keyboard.A)
+        dKey=game.input.keyboard.addKey(Phaser.Keyboard.D)
 
 	  }
 
 	  function update(){
 	  	game.physics.arcade.collide(player,platforms);
 	  	game.physics.arcade.collide(stars,platforms);
-	  	ame.physics.arcade.collide(enemeny,platforms);
+	  	game.physics.arcade.collide(enemeny,platforms);
 
-	  	player.body.verlocity.x=0
-	  	if(akey.isDown){
-	  		player.body.verlocity.x = -150;
+	  	player.body.velocity.x=0
+	  	if(aKey.isDown){
+	  		player.body.velocity.x = -150;
 	  		player.animations.play("left");
             
-	  	} else if (cursors.right.isDown|| dkey.isDown){
-	  	player.body.verlocity.x=150;
+	  	} else if (cursors.right.isDown|| dKey.isDown){
+	  	player.body.velocity.x=150;
 	  	player.animations.play("right");
 
 	  }else {
-	  	player.animations.stop():
+	  	player.animations.stop();
 	  	player.frame= 4;
 
 	  }
-	  if(cursors.up.isDown || wKey.is down && player.body.touching.down){
+	  if(wKey.isDown && player.body.touching.down){
 
-	  	player.body.verlocity.y= -300;
+	  	player.body.velocity.y= -300;
+
 	  }
+	  //overlapes
+	  game.physics.arcade.overlap(player,stars,collectStar);
+	  game.physics.arcade.overlap(player,enemeny,loseLive);
+
+	  moveEnemy();
+
+	  if(lives <-1){
+	  	endGame();
+	  }
+
+
+}
+function collectStar(player,star ){
+	//add to score
+	score++;
+	scoretext.setText(score);
+
+	//star to reset
+	star.kill();
+	star.reset(Math.random()*750,0);
+
 }
 
+function loseLive(player,enemeny){
+	//- live
+	lives--;
+	livestext.setText(lives);
+
+	enemeny.kill();
+	enemeny.reset(Math.random()*750,0);
+
+}
+
+function moveEnemy(){
+	if(enemeny.x >739){
+		enemeny.animations.play('left');
+		enemeny.body.velocity.x=-120;
+
+	}else if(enemeny.x<400){
+		enemeny.animations.play('right');
+		enemeny.body.velocity.x=120;
+
+
+	}
+
+}
+function endGame(){
+	player.kill();
+	liveslabel.visible = false;
+	livestext.visible= false;
+	scoretext.visible=false;
+	scorelabel.text ="GAME OVER! You Scored" + score;
+
+
+}
